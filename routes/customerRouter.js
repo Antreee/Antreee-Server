68e6a70c6a816f7e5ef9af77b5780b30e7a7dc9e
arda <fatikaardha@gmail.com>
2022-04-13 20:38:35 +0800

    customers crud

--- routes/customerRouter.js
+++ routes/customerRouter.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const UserController = require("../controllers/userController");
+
+router.post("/", UserController.createUser)
+router.get("/", UserController.fetchUsers)
+router.get("/:id", UserController.findUser)
+router.delete("/:id", UserController.deleteUser)
+router.put("/:id", UserController.updateUser)
 
 module.exports = router;
