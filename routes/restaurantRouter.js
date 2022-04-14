300c7b70376508d57fcc18a78cc6b4cf08d2e953
Rasyid619 <arifinrasyid13@gmail.com>
2022-04-14 23:50:23 +0700

    get restaurant by Id

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -3,5 +3,6 @@ const RestaurantController = require("../controllers/restaurantController");
 
 router.post("/", RestaurantController.createRestaurant);
 router.get("/", RestaurantController.fetchRestaurant);
+router.get("/:id", RestaurantController.fetchRestaurantById);
 
 module.exports = router;
