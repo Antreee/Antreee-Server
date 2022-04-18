4903de358957438e3a25d5edb3cbbe7eaa8bd566
Rasyid619 <arifinrasyid13@gmail.com>
2022-04-18 23:03:13 +0700

    fix restaurant schema

--- models/order.model.js
+++ models/order.model.js
@@ -1,39 +1,41 @@
 const mongoose = require("mongoose");
 const connection = require("../config/mongodb");
 // const OrderDetail = require("./orderDetail.model");
+const Restaurant = require("./restaurant.model");
 
 const order = new mongoose.Schema({
-  customerName: {
-    type: String,
-  },
-  customerPhoneNumber: {
-    type: String,
-  },
-  tableNumber: {
-    type: String,
-  },
-  totalPrice: {
-    type: Number,
-    required: true,
-  },
-  bookingDate: {
-    type: String,
-  },
-  numberOfPeople: {
-    type: String,
-  },
-  status: {
-    type: String,
-  },
-  customerEmail: {
-    type: String,
-    required: true,
-  },
-
-  // orderDetails: [{
-  //     ref: OrderDetail,
-  //     type: mongoose.Schema.Types.ObjectId,
-  // }],
+	customerName: {
+		type: String,
+	},
+	customerPhoneNumber: {
+		type: String,
+	},
+	tableNumber: {
+		type: String,
+	},
+	totalPrice: {
+		type: Number,
+		required: true,
+	},
+	bookingDate: {
+		type: String,
+	},
+	numberOfPeople: {
+		type: String,
+	},
+	status: {
+		type: String,
+	},
+	customerEmail: {
+		type: String,
+		required: true,
+	},
+	restaurantId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: Restaurant,
+		required: true,
+		index: true,
+	},
 });
 
 const Order = connection.model("Order", order);
