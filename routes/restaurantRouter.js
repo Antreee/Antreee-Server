9e2546153482953879c3d27bf5c8b311a9451e56
arda <fatikaardha@gmail.com>
2022-04-18 14:31:13 +0800

    authentication

--- routes/restaurantRouter.js
+++ routes/restaurantRouter.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const RestaurantController = require("../controllers/restaurantController");
 const FoodController = require("../controllers/itemController");
+const authentication = require("../middlewares/authentication")
+
 
 router.post("/", RestaurantController.createRestaurant);
+router.get("/admin", authentication, RestaurantController.getRestaurantByAdminId)
 router.get("/", RestaurantController.fetchRestaurant);
 router.get("/:id/items", FoodController.getItemByRestaurantId);
 router.get("/:id", RestaurantController.fetchRestaurantById);
