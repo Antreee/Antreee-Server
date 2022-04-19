af9eeaa2c3dde09fddad9ffb64d4253502db7275
Rasyid619 <arifinrasyid13@gmail.com>
2022-04-20 01:20:32 +0700

    get order by restaurant id

--- models/order.model.js
+++ models/order.model.js
@@ -1,6 +1,4 @@
 const mongoose = require("mongoose");
-// const connection = require("../config/mongodb");
-// const OrderDetail = require("./orderDetail.model");
 const Restaurant = require("./restaurant.model");
 
 const order = new mongoose.Schema({
