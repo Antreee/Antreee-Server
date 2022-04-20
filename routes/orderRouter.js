f81754e02b230cc92f8ee9a3c1625caaa3ea3cfb
Aditya FH <aditfurkon2000@gmail.com>
2022-04-20 20:41:58 +0700

    order by id endpoint

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const orderController = require("../controllers/orderController");
 
 // router.get("/", orderController.fetchOrder);
-// router.get("/details/:id", orderController.fetchOrderDetails);
+router.get("/:id", orderController.fetchOrderById);
 
 module.exports = router;
