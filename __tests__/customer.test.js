cd1d75f1fc6a9cd4837f4c3f0f75b81c7f74c3f7
Rasyid619 <arifinrasyid13@gmail.com>
2022-04-20 14:31:32 +0700

    tdd done

--- __tests__/customer.test.js
+++ __tests__/customer.test.js
@@ -5,78 +5,78 @@ const Order = require("../models/order.model");
 const OrderDetail = require("../models/orderDetail.model");
 
 beforeAll(async () => {
-  try {
-    await mongoose.connect("mongodb://localhost:27017/test_NuerPay");
-  } catch (error) {
-    console.log(error);
-  }
+	try {
+		await mongoose.connect("mongodb://localhost:27017/test_NuerPay");
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 afterAll(async () => {
-  await Order.deleteMany();
-  await OrderDetail.deleteMany();
-  await mongoose.disconnect();
+	await Order.deleteMany();
+	await OrderDetail.deleteMany();
+	await mongoose.disconnect();
 });
 
 describe("create order", () => {
-  describe("POST /customers/orders - success test without booking date", () => {
-    it("should return a string with status 201", async () => {
-      const res = await request(app)
-        .post("/customers/orders")
-        .send({
-          customerName: "adit19",
-          customerPhoneNumber: "083647584938",
-          customerEmail: "adit@mail.com",
-          tableNumber: "A-12",
-          totalPrice: 500000,
-          bookingDate: "",
-          numberOfPeople: "",
-          restaurantId: "625a63636ab2f903486ecc74",
-          orderDetails: [
-            {
-              itemId: "62583b12aa7f55aa593dcacd",
-              quantity: 100,
-            },
-            {
-              itemId: "6257090a69627e5b51c559a6",
-              quantity: 99,
-            },
-          ],
-        });
-      expect(res.status).toBe(201);
-      expect(res.body).toEqual(expect.any(String));
-    });
-  });
-  describe("POST /customers/orders - success test with booking date and without order details", () => {
-    it("should return 'Booking Success' with status 200", async () => {
-      const res = await request(app).post("/customers/orders").send({
-        customerName: "adit19",
-        customerPhoneNumber: "083647584938",
-        customerEmail: "adit@mail.com",
-        bookingDate: "19/04/2022 22:52",
-        numberOfPeople: "4",
-        restaurantId: "625a63636ab2f903486ecc74",
-      });
-      expect(res.status).toBe(200);
-      expect(res.body).toBe("Booking Success");
-    });
-  });
-  describe("POST /customers/orders - failed test without restaurant id", () => {
-    it("should return an object with status 400", async () => {
-      const res = await request(app).post("/customers/orders").send({
-        customerName: "adit19",
-        customerPhoneNumber: "083647584938",
-        customerEmail: "adit@mail.com",
-        bookingDate: "19/04/2022 22:52",
-        numberOfPeople: "4",
-      });
-      expect(res.status).toBe(400);
-      expect(res.body).toHaveProperty("messages");
-      expect(res.body).toHaveProperty("fields", "restaurantId is required");
-      expect(res.body.messages).toBeInstanceOf(Array);
-      expect(res.body.messages).toEqual(
-        expect.arrayContaining(["Path `restaurantId` is required."])
-      );
-    });
-  });
+	describe("POST /customers/orders - success test without booking date", () => {
+		it("should return a string with status 201", async () => {
+			const res = await request(app)
+				.post("/customers/orders")
+				.send({
+					customerName: "adit19",
+					customerPhoneNumber: "083647584938",
+					customerEmail: "adit@mail.com",
+					tableNumber: "A-12",
+					totalPrice: 500000,
+					bookingDate: "",
+					numberOfPeople: "",
+					restaurantId: "625a63636ab2f903486ecc74",
+					orderDetails: [
+						{
+							itemId: "62583b12aa7f55aa593dcacd",
+							quantity: 100,
+						},
+						{
+							itemId: "6257090a69627e5b51c559a6",
+							quantity: 99,
+						},
+					],
+				});
+			expect(res.status).toBe(201);
+			expect(res.body).toEqual(expect.any(String));
+		});
+	});
+	describe("POST /customers/orders - success test with booking date and without order details", () => {
+		it("should return 'Booking Success' with status 200", async () => {
+			const res = await request(app).post("/customers/orders").send({
+				customerName: "adit19",
+				customerPhoneNumber: "083647584938",
+				customerEmail: "adit@mail.com",
+				bookingDate: "19/04/2022 22:52",
+				numberOfPeople: "4",
+				restaurantId: "625a63636ab2f903486ecc74",
+			});
+			expect(res.status).toBe(200);
+			expect(res.body).toBe("Booking Success");
+		});
+	});
+	describe("POST /customers/orders - failed test without restaurant id", () => {
+		it("should return an object with status 400", async () => {
+			const res = await request(app).post("/customers/orders").send({
+				customerName: "adit19",
+				customerPhoneNumber: "083647584938",
+				customerEmail: "adit@mail.com",
+				bookingDate: "19/04/2022 22:52",
+				numberOfPeople: "4",
+			});
+			expect(res.status).toBe(400);
+			expect(res.body).toHaveProperty("messages");
+			expect(res.body).toHaveProperty("fields", "restaurantId is required");
+			expect(res.body.messages).toBeInstanceOf(Array);
+			expect(res.body.messages).toEqual(
+				expect.arrayContaining(["Path `restaurantId` is required."])
+			);
+		});
+	});
 });
--- middlewares/errorhandler.js
+++ middlewares/errorhandler.js
@@ -1,53 +1,40 @@
-const { JsonWebTokenError } = require("jsonwebtoken");
-
-//handle email or usename duplicates
-const handleDuplicateKeyError = (err, res) => {
-	const field = Object.keys(err.keyValue);
-	const code = 409;
-	const error = `An account with that ${field} already exists.`;
-	res.status(code).send({ messages: error, fields: field });
-};
-//handle field formatting, empty fields, and mismatched passwords
-const handleValidationError = (err, res) => {
-	let errors = Object.values(err.errors).map((el) => el.message);
-	let fields = Object.values(err.errors).map((el) => el.path);
-	let code = 400;
-	if (errors.length > 1) {
-		const formattedErrors = errors.join("");
-		res
-			.status(code)
-			.send({ messages: formattedErrors, fields: `${fields} is required` });
-	} else {
+module.exports = (err, req, res, next) => {
+	console.log("congrats you hit the error middleware");
+	console.log(err.name, "<<<", err, "<<<");
+	if (err.name === "ValidationError") {
+		let errors = Object.values(err.errors).map((el) => el.message);
+		let fields = Object.values(err.errors).map((el) => el.path);
+		let code = 400;
+		// if (errors.length > 1) {
+		// 	const formattedErrors = errors.join("");
+		// 	res
+		// 		.status(code)
+		// 		.send({ messages: formattedErrors, fields: `${fields} is required` });
+		// } else {
 		res
 			.status(code)
 			.send({ messages: errors, fields: `${fields} is required` });
-
-	}
-};
-
-module.exports = (err, req, res, next) => {
-	try {
-		console.log("congrats you hit the error middleware");
-		console.log(err.name, "<<<", err, "<<<");
-		if (err.name === "ValidationError")
-			return (err = handleValidationError(err, res));
-		if (err.code && err.code == 11000)
-			return (err = handleDuplicateKeyError(err, res));
-		if (err.message === "Invalid email or password")
-			return res.status(400).send({ message: err.message });
-		if (err.name === "InvalidUser")
-			return res.status(401).send({ message: err.message });
-		if (err.name === "NotFound")
-			return res.status(404).send({ message: err.message });
-		if (err.name === "BadRequest")
-			return res.status(400).send({ message: err.message });
-		if (err.name === "BSONTypeError")
-			return res.status(400).send({ message: "Invalid order id" });
-		if (err.name === "JsonWebTokenError")
-			return res.status(401).send({ message: "Invalid Token" });
-    if (err.name === "CastError") return res.status(400).send({ message: "Invalid restaurant id" });
-		if (err.name === "TypeError") return res.status(400).send({ message: "Invalid restaurant id" });
-	} catch (err) {
-		res.status(500).send("An unknown error occurred.");
-	}
+		// }
+		// } else if (err.code && err.code == 11000) {
+		// 	const field = Object.keys(err.keyValue);
+		// 	const code = 409;
+		// 	const error = `An account with that ${field} already exists.`;
+		res.status(code).send({ messages: error, fields: field });
+	} else if (err.message === "Invalid email or password")
+		return res.status(400).send({ message: err.message });
+	else if (err.name === "InvalidUser")
+		return res.status(401).send({ message: err.message });
+	else if (err.name === "NotFound")
+		return res.status(404).send({ message: err.message });
+	else if (err.name === "BadRequest")
+		return res.status(400).send({ message: err.message });
+	else if (err.name === "BSONTypeError")
+		return res.status(400).send({ message: "Invalid order id" });
+	else if (err.name === "JsonWebTokenError")
+		return res.status(401).send({ message: "Invalid Token" });
+	else if (err.name === "CastError")
+		return res.status(400).send({ message: "Invalid restaurant id" });
+	else if (err.name === "TypeError")
+		return res.status(400).send({ message: "Invalid restaurant id" });
+	else return res.status(500).send("An unknown error occurred.");
 };
